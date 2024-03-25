efa4c18ee053e9ae627a7564f75d0003af6feec9
crismiche1 <crisnat1905@gmailcom>
2024-03-25 12:09:47 -0300

    configuramos conexion a base de datos

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-
 const connectDB = async () => {
     try {
-
         // Conexión a la base de datos MongoDB
-        await mongoose.connect(process.env.MONGODB_URL, {dbName: process.env.MONGODB_NAME , family: 4,});
+        await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}`, {
+         
+            family: 4
+        });
 
         console.log('Conexión a MongoDB exitosa');
     } catch (error) {
         console.error('Error de conexión a MongoDB:', error);
-       
     }
 };
 
